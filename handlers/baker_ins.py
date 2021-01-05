6c66e1c79f9149c7fcea7d5acc0d8c3656121e71
David <david.enriquez@zx-ventures.mx>
2021-01-04 19:34:56 -0600

    Add baker institute

---
+++ handlers/baker_ins.py
@@ -0,0 +1,38 @@
+import json
+import datetime
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+end_date=datetime.date.today()
+start_date=(end_date - datetime.timedelta(days=14))
+start_datetime = datetime.datetime.combine(start_date, datetime.datetime.min.time())
+publisher='Baker Institute'
+
+def get_data ():
+    #Baker Institute principal
+    results=[]
+    url_base='https://s6.searchcdn.com/77d3d7d0562eef506e6ea07527cdb14e/mexico/0/539/?s=1609807824586&sort=doc_date&order=desc&resultCount=100'
+    response=requests.get(url_base)
+    page=response.text
+    page=page.replace('addsearch.searchResults(', '')
+    page=page.replace(');', '')
+    documents=json.loads(page)
+    articles=(documents['es']['hits']['hits'])
+    for i, val in enumerate(articles):
+        _date=datetime.datetime.strptime(val['fields']['doc_date'], '%Y-%m-%dT%H:%M:%S')
+        if _date > start_datetime:
+            if 'title' in val['fields']:
+                _title=val['fields']['title']
+            else:
+                _title=val['fields']['url']
+            myDict={
+                'title': _title,
+                'url': val['fields']['url'],
+                'date': val['fields']['doc_date'],
+                'publisher': publisher
+            }
+            results.append(myDict)
+    return results
+
+get_data()
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from handlers import aei
 from handlers import atlantic_council
+from handlers import baker_ins
 import pandas as pd
 
 atlantic_council_data=atlantic_council.get_data()
 aei_data=aei.get_data()
+baker_ins_data=baker_ins.get_data()
 
 main_data=pd.DataFrame()
 main_data=main_data.append(atlantic_council_data)
 main_data=main_data.append(aei_data)
+main_data=main_data.append(baker_ins_data)
 
 main_data.to_csv('./outputs/main_data.csv', index=False)
